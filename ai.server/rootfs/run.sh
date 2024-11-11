98f68a0d50cc2bc0766d761ea54107942f082ab8
Vitalii Smirnov <vsmirnov@me.com>
2024-11-11 12:25:47 +0300

    data dir fixes

--- ai.server/rootfs/run.sh
+++ ai.server/rootfs/run.sh
@@ -6,7 +6,7 @@ export DATA_DIR=$(jq --raw-output '.DATA_DIR // empty' $CONFIG_PATH)
 mkdir -p "$MODULES_PATH"
 mkdir -p "$DATA_DIR"
 
-cat /app/server/appsettings.json  | sed -e 's/\/\/ .*$//g' | jq ".ModuleOptions.ModulesPath=\"$MODULES_PATH\"" > /app/server/appsettings.json.new
+cat /app/server/appsettings.json  | sed -e 's/\/\/ .*$//g' | jq ".ModuleOptions.ModulesDirPath=\"$MODULES_PATH\"" > /app/server/appsettings.json.new
 mv /app/server/appsettings.json.new /app/server/appsettings.json
 
 cd /app/server && dotnet ./CodeProject.AI.Server.dll --ApplicationDataDir="$DATA_DIR"
